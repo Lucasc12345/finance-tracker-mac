8d4d687278985f3ad658c9680d9596a489267e10
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-11-07 09:46:35 +0100

    Modificato lìil controller stock per farlo uguale al corso e modificato il modello Stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,6 +4,9 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         render 'users/my_portfolio'
+      elsif @stock == 0
+        flash[:alert] = "You have reached the maximum limit of 25 API requests."
+        redirect_to my_portfolio_path
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,27 +1,35 @@
 require 'httparty'
-
 class Stock < ApplicationRecord
-  def self.company_lookup(ticker_symbol)
-    response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_vantage[:api_key]}")
-    if response.code == 200
-      response.parsed_response['Name']
-    else
-      nil
-    end
-  end
-
   def self.new_lookup(ticker_symbol)
     begin
-      response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_vantage[:api_key]}")
-      if response.code == 200 && response.parsed_response['Global Quote']
-        last_price = response.parsed_response['Global Quote']['05. price']
-        name = company_lookup(ticker_symbol)
-        new(ticker: ticker_symbol, name: name, last_price: last_price)
-      else
-        nil
-      end
+      price = fetch_price(ticker_symbol)
+      return 0 if price == 0
+      return nil unless price
+      name = company_lookup(ticker_symbol)
+      return nil unless name
+      create_stock(ticker_symbol, name, price)
     rescue => exception
       nil
     end
   end
+
+  private
+  def self.fetch_price(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_vantage[:api_key]}")
+    if response.code == 200 && response.parsed_response.dig('Global Quote', '05. price')
+      BigDecimal(response.parsed_response['Global Quote']['05. price'])
+    elsif response.parsed_response['Information']
+      0
+    else
+      nil
+    end
+  end
+  def self.company_lookup(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_vantage[:api_key]}")
+    response.parsed_response['Name'] if response.code == 200 && response.parsed_response['Name']
+  end
+  def self.create_stock(ticker_symbol, name, price)
+    new(ticker: ticker_symbol, name: name, last_price: price)
+  end
 end
+
