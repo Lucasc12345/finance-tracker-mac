2b919bba0be7ea466b43a7b8fc33af05089c3a4a
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-11-08 18:40:13 +0100

    Correzzione

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,8 @@ class FriendshipsController < ApplicationController
   def create
   end
   def destroy
-    @friendship = current_user.friendships.where(friend_id: params[:id]).first
-    @friendship.destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship&.destroy
     flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
