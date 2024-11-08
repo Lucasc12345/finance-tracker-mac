8aec0a65db3527fc9f21b43f167aa9fd494ad8a3
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-11-08 10:45:11 +0100

    Aggiunti operatori & per eviatare l'errore segnalato dall'IDE

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -3,10 +3,10 @@ class UserStocksController < ApplicationController
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
-      stock.save
+      stock&.save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
-    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    flash[:notice] = "Stock #{stock&.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
 end
